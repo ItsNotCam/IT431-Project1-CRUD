8bb966801b9f55e6fdc6d5c3ff8cad9d7a158b21
ItsNotCam <samspwrsd@gmail.com>
2025-03-04 11:04:47 -0500

    updating homepage to emulate carmax

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
 import { useState } from 'react'
 
+import CarMaxLogo from "./assets/CarmaxLogo.svg"
+import LocationIcon from "./assets/LocationIcon.svg"
+import PersonIcon from "./assets/PersonIcon.svg"
+import HeartIcon from "./assets/HeartIcon.svg"
+import SearchIcon from "./assets/SearchIcon.svg"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1 className='text-3xl text-black'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+		<nav className='w-full flex gap-8 h-12 my-4 px-4 items-center'>
+			<img src={CarMaxLogo} alt="Carmax Logo" className='h-8 mt-2'/>
+			<div className='flex flex-grow gap-10 text-[#053361]'>
+				<a href="" className=''>Shop</a>
+				<a href="" className=''>Sell/Trade</a>
+				<a href="" className=''>Finance</a>
+				<a href="" className=''>Research</a>
+			</div>
+			<div className='ml-auto mr-4 flex gap-6 items-center'>
+				<img src={LocationIcon} alt="Location Icon" className='h-6'/>
+				<img src={HeartIcon} alt="Heart Icon" className='h-6 ml-2'/>
+				<img src={PersonIcon} alt="Person Icon" className='h-8'/>
+			</div>
+		</nav>
+		<div className='grid grid-cols-2 mx-auto my-4'>	
+			<input type="text" className='
+				h-12 border border-gray-300 rounded-lg px-4 mx-8 bg-[#f5f6f7]
+				col-start-1 col-end-3 row-start-1 row-end-2
+			'/>
+			<img src={SearchIcon} alt="Search Icon" className='
+				h-6 col-start-2 col-end-3 row-start-1 row-end-2 my-auto 
+				ml-auto mr-12 pointer-events-none
+			'/>
+		</div>
+		<div className="">
+			<h1 className="text-xl font-bold ml-8 mt-6">Used Cars</h1>
+		</div>
     </>
   )
 }
